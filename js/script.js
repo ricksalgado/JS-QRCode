154c7d09c0b19b7ce385036483275df98cea8d55
Ricardo Salgado <ricardodsalg@gmail.com>
2024-12-23 18:46:38 -0500

    Testing JS button

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const container = document.querySelector('.container');
+const qrCodeBtn = document.querySelector('#qr-form button');
+
+// Eventos
+function generateQR(){
+    console.error('acionou legal')
+}
+
+qrCodeBtn.addEventListener('click', () => {
+    generateQR()
+})
