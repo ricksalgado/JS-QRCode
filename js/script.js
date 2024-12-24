09e964f0e1de652046bd71b0cce10c08fc33cdd4
Ricardo Salgado <ricardodsalg@gmail.com>
2024-12-23 19:04:37 -0500

    QRCode v1
    The code is working.
    Now I need to work on the best presentation

--- js/script.js
+++ js/script.js
@@ -3,13 +3,20 @@ const qrCodeBtn = document.querySelector('#qr-form button');
 
 const qrCodeInput = document.querySelector('#qr-form input')
 
+const qrCodeImg = document.querySelector('#qr-code img')
+
 // Eventos
 function generateQR(){
 
     const qrCodeInputValue = qrCodeInput.value
 
-    console.log(qrCodeInputValue);
+    if(!qrCodeInputValue) return;
 
+    qrCodeBtn.innerHTML = 'Generating image...';
+
+    qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrCodeInputValue}`
+
+    container.classList.add('active')
 }
 
 qrCodeBtn.addEventListener('click', () => {
